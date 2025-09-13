273b49fbc917420d3daf897eea62e09ba777f9af
861995 <vk18rp08@gmail.com>
2025-09-13 18:05:47 +0530

    test: Multiple num addition by detecting ","

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -6,6 +6,12 @@ void main() {
     expect(calculator.add(""), 0);
     expect(calculator.add("1"), 1);
     expect(calculator.add("2"), 2);
+    expect(calculator.add("3"), 3);
+    expect(calculator.add("4"), 4);
+    expect(calculator.add("1,2"), 3);
+    expect(calculator.add("1,25"), 26);
+    expect(calculator.add("100,250"), 350);
+    expect(calculator.add("1,2,3,4,5"), 15);
   });
 }
 
@@ -13,8 +19,14 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) {
       return 0;
-    } else {
-      return int.parse(numbers);
+    } else if (numbers.contains(',')) {
+      List<String> splittedNum = numbers.split(',');
+      List<int> nums = [];
+      for (var splitNum in splittedNum) {
+        nums.add(int.parse(splitNum));
+      }
+      return nums.fold(0, (sum, element) => sum + element);
     }
+    return int.parse(numbers);
   }
 }

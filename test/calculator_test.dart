28216083438cc696d4c57321102168698905603c
861995 <vk18rp08@gmail.com>
2025-09-13 17:30:35 +0530

    test: Test case for single digit

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -4,11 +4,17 @@ void main() {
   test('Calculator test', () {
     final calculator = StringCalculator();
     expect(calculator.add(""), 0);
+    expect(calculator.add("1"), 1);
+    expect(calculator.add("2"), 2);
   });
 }
 
 class StringCalculator {
   int add(String numbers) {
-    return 0;
+    if (numbers.isEmpty) {
+      return 0;
+    } else {
+      return int.parse(numbers);
+    }
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,8 @@
 // This is a basic Flutter widget test.
 
-import 'package:flutter_test/flutter_test.dart';
-
-import 'calculator_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+//
+// import 'calculator_test.dart';
 
 // void main() {
 //   test('Calculator test', () {
